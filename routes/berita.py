1db175999d14988635f0fddec6b7771b01f7914b
maulanairfanf <maulanairfanf@gmail.com>
2022-03-31 12:04:46 +0700

    create filter date

--- routes/berita.py
+++ routes/berita.py
@@ -15,5 +15,9 @@ async def filter_category(category : str):
     return conn.execute(list_berita.select().where(list_berita.c.category == category)).fetchall()
 
 @berita.get("/website/{website}")
-async def filter_websote(website : str):
+async def filter_website(website : str):
     return conn.execute(list_berita.select().where(list_berita.c.website == website)).fetchall()
+
+@berita.get("/date/{date}")
+async def filter_date(date : str):
+    return conn.execute(list_berita.select().where(list_berita.c.date == date)).fetchall()
